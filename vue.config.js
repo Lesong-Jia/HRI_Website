075f99d60f40474837a69b47d59512ad212a4db6
LELEJIA <LELEJIA@users.noreply.github.com>
2024-02-13 01:41:32 -0500

    e

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
-  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '',
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'dist',
   // eslint-loader 是否在保存的时候检查
   lintOnSave: false,
   /**
